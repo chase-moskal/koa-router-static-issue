8d9a91ef591c529d9ece484e4c7c07e0cb74e55e
Chase Moskal <chasemoskal@gmail.com>
2019-01-21 10:26:08 -0800

    demonstrating the functional/working condition

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const Router = require("koa-router")
 const app = new Koa()
 const router = new Router()
 
-// fails with 404... why?
-router.use(serve("public"))
+// // fails with 404... why?
+// router.use(serve("public"))
 
-// // this, on the other hand, works
-// app.use(serve("public"))
+// this, on the other hand, works
+app.use(serve("public"))
 
 app.use(router.middleware())
 app.listen(8080)
