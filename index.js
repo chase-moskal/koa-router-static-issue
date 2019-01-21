d993f3fc4dea31cbe1b1013385eedea634151af7
Chase Moskal <chasemoskal@gmail.com>
2019-01-21 10:24:56 -0800

    initial commit, in broken condition

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const Koa = require("koa")
+const serve = require("koa-static")
+const Router = require("koa-router")
+
+const app = new Koa()
+const router = new Router()
+
+// fails with 404... why?
+router.use(serve("public"))
+
+// // this, on the other hand, works
+// app.use(serve("public"))
+
+app.use(router.middleware())
+app.listen(8080)
